a2af1718c2f76a16a2fd1f4158be47841a5feec0
elaa <elaa.brahmii@gmail.com>
2025-06-22 19:13:34 +0100

    add auth logic to generator

--- app/(logged-in)/generator/page.tsx
+++ app/(logged-in)/generator/page.tsx
@@ -1,4 +1,28 @@
+"use client"
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../../../utils/firebaseConfig';
+import React, { useState, useEffect } from 'react';
+import { redirect } from "next/navigation";
 export default function Generator(){
+
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              // User is signed in, see docs for a list of available properties
+              // https://firebase.google.com/docs/reference/js/firebase.User
+              console.log(user);
+              const uid = user.uid;
+              // ...
+              console.log("uid", uid)
+            } else {
+              // User is signed out
+              // ...
+              console.log("user is logged out")
+              redirect('/sign-up');
+            }
+          });
+         
+    }, [])
     return(
         <div>generator</div>
     )
