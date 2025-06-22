26535ea86db6965891ded61ce04a21c07283fbb7
elaa <elaa.brahmii@gmail.com>
2025-06-22 19:12:30 +0100

    changed names in env file

---
+++ utils/firebaseConfig.ts
@@ -0,0 +1,18 @@
+"use client"
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth=getAuth(app);
+console.log(auth);
+//export default app;
