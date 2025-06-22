84fdcea4ea10646f321f5ca466139ac4be0cb3a7
elaa <elaa.brahmii@gmail.com>
2025-06-22 22:46:59 +0100

    add firestorage

---
+++ lib/firebase/firebaseConfig.ts
@@ -0,0 +1,29 @@
+"use client"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const usersCollection = collection(db, "users");
+const auth=getAuth(app);
+console.log(auth);
+export{
+  app,
+  auth,
+  db,
+  storage,
+  usersCollection
+}
+//export default app;
