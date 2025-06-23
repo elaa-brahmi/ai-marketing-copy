81af2cb60f37d25e3875caa07991785a9bfb3fc4
elaa <elaa.brahmii@gmail.com>
2025-06-23 11:33:48 +0100

    sign up is ready to go

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -12,10 +12,13 @@ import { signInWithGoogle } from "@/lib/firebase/auth";
 import { addDoc, serverTimestamp } from "firebase/firestore";
 import { usersCollection } from "../../lib/firebase/firebaseConfig";
 import { saveUser, getUser } from "../../lib/users";
+import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import { Lock, Mail, User } from "lucide-react";
+import Link from "next/link";
 export default function SignUp() {
   const router = useRouter();
   const [createUser] = useCreateUserWithEmailAndPassword(auth);
-  const [sendEmailVerification] = useSendEmailVerification(auth);
+ // const [sendEmailVerification] = useSendEmailVerification(auth);
   const [signInWithGoogle] = useSignInWithGoogle(auth);
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
@@ -33,11 +36,13 @@ export default function SignUp() {
           account_status: 'active'
         };
         await saveUser(userData);
-        await sendEmailVerification();
+        if (userCredential && userCredential.user) {
+          await sendEmailVerification(userCredential.user);
+        }
         router.push("/");
       }
     } catch (e) {
-      console.error(e);}
+      console.error("sign up error ",e);}
     setLoading(false);
   };
 
@@ -54,7 +59,8 @@ export default function SignUp() {
           account_status: 'active'
         };
         await saveUser(userData);
-        await sendEmailVerification();
+         // Only send verification for email/password users
+       
         router.push("/");
       }
     } catch (e) {
@@ -64,40 +70,62 @@ export default function SignUp() {
   };
 
   return (
-    <div className="flex justify-center items-center flex-col">
-      <h1>Create account</h1>
-      {loading ? (
-        <IconFidgetSpinner className="animate-spin w-8 h-8" />
-      ) : (
-        <>
-          <input
-            type="text"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            placeholder="Email"
-            className="text-xl px-4 py-2 rounded-md border border-gray-300 mb-4"
-          />
-          <input
-            type="text"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            placeholder="Password"
-            className="text-xl px-4 py-2 rounded-md border border-gray-300 mb-4"
-          />
-          <button
-            className="cursor-pointer bg-yellow-500 text-black px-4 py-2 rounded-md font-bold"
-            onClick={onSubmit}
-          >
-            SIGN UP
-          </button>
-          <button
-            className="cursor-pointer bg-blue-500 text-white px-4 py-2 mt-4 rounded-md font-bold"
-            onClick={handleGoogleSignIn}
-          >
-            Sign In with Google
-          </button>
-        </>
-      )}
+    <div className="bg-violet-100/60 h-dvh py-5 w-xl px-5 rounded-xl mx-auto flex justify-center items-center flex-col">
+      <div className=" h-full w-96 text-center">
+        <span  className="mx-auto h-9 w-9 flex justify-center items-center rounded bg-blue-100 ">
+          <User className="h-6 w-6 text-violet-700"/>
+        </span>
+        <h1 className="text-center font-bold sm:text-2xl my-3 md:text-3xl">Create account</h1>
+        <p className="text-gray-500">Enter your information to get started</p>
+        {loading ? (
+          <IconFidgetSpinner className="animate-spin w-8 h-8 mt-5" />
+        ) : (
+          <>
+            <button
+              className="cursor-pointer my-3 bg-white text-black w-xs p-4 rounded-md font-bold"
+              onClick={handleGoogleSignIn}
+            >
+              Sign In with Google
+            </button>
+           {/*  <span>
+              <p className="text-gray-500 capitalize mb-3">Or continue with email</p>
+            </span> */}
+            <div className="mb-10 mt-4 flex items-center justify-center gap-4 text-gray-500 text-sm">
+              <hr className="flex-grow border-t border-gray-300" />
+              <span className="whitespace-nowrap">OR CONTINUE WITH EMAIL</span>
+              <hr className="flex-grow border-t border-gray-300" />
+            </div>
+            <span>
+            <Mail className="w-5 h-5  inline absolute ms-2 mt-3 text-gray-500 "/>
+            <input
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              placeholder="Enter your Email"
+              className="focus:outline-none focus:ring-2 focus:ring-violet-500 transition-colors duration-200 ps-9 w-xs text-xl px-4 py-2 rounded-md border border-gray-300 mb-4"
+            /></span>
+            <span>
+            <Lock className=" w-5 h-5 absolute ms-10 mt-3  text-gray-500 "/>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              placeholder="Create a Password"
+              className="focus:outline-none focus:ring-2 focus:ring-violet-500 transition-colors duration-200 ps-9  w-xs text-xl px-4 py-2 rounded-md border border-gray-300 mb-4"
+            /></span>
+            <button
+              className="w-xs my-3 cursor-pointer bg-violet-500 text-white p-4  rounded-md font-bold"
+              onClick={onSubmit}
+            >
+            Create account
+            </button>
+            <p className="mt-7 text-gray-500">Already have an account? <span className="text-violet-500 cursor-pointer" >
+              <Link href="/sign-in"> Sign in</Link>
+             </span></p>
+          
+          </>
+        )}
+      </div>
     </div>
   );
 }
