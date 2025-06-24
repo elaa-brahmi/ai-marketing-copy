e6012beccfc5ef5c39596c213469c9decd7a12d6
elaa <elaa.brahmii@gmail.com>
2025-06-24 11:09:22 +0100

    +password verification

--- lib/users.ts
+++ lib/users.ts
@@ -8,10 +8,18 @@ export type User = {
   password: string; // Do NOT store plain passwords in production!
   account_status: string;
 };
+import { collection, query, where } from "firebase/firestore";
 
 // Save user to Firestore
 export async function saveUser(user: User) {
-  await setDoc(doc(db, "users", user.userId), user);
+  //check whether the user already exists
+  const userRef = doc(db, "users", user.userId);
+  const userSnap = await getDoc(userRef);
+  if (!userSnap.exists()) {
+    await setDoc(userRef, user);
+    return true; // User was created
+  }
+  return false; // User already exists
 }
 
 // Retrieve user from Firestore
@@ -21,4 +29,22 @@ export async function getUser(userId: string): Promise<User | null> {
     return userDoc.data() as User;
   }
   return null;
+}
+export async function getUserByEmail(email: string): Promise<boolean | null> {
+  const q = query(collection(db, "users"), where("user_email", "==", email));
+  const querySnapshot = await getDocs(q);
+  if (!querySnapshot.empty) {
+    return true;
+  }
+  return false;
+}
+export async function verifyPassword(email:string,password:string): Promise<boolean | null>{
+  const q = query(collection(db, "users"), where("user_email", "==", email),
+  where("password","==",password));
+  const querySnapshot = await getDocs(q);
+  if (!querySnapshot.empty) {
+    return true;
+  }
+  return false;
+
 }
