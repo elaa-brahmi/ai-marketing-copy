0367c316f0b0b15f93b116687cc64e21e4a38bdf
elaa <elaa.brahmii@gmail.com>
2025-06-28 11:24:37 +0100

    remove set cookie

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import type { NextRequest } from 'next/server';
 // This function checks for a Firebase ID token in cookies
 export function middleware(request: NextRequest) {
   const idToken = request.cookies.get('firebase_id_token')?.value;
-
   // If no token, redirect to sign-up
   if (!idToken) {
     console.log('No token found, redirecting to sign-up');
